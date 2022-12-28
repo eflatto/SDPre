33563f7b8e2825f8aa18b1c6207813b05f3ccc39
Edwin <Flattoedwin@gmail.com>
2022-12-28 00:52:46 -0500

    complete objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year = 1997;
+    String month = "August";
+    boolean todayIsAWeekend = false;
+
+    System.out.println(year+"\n" + month +"\n" + todayIsAWeekend);
+
+  }
+}
