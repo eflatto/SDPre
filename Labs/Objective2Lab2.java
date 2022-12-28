b606991e7960d50da093c3429ba754a27633eb83
Edwin <Flattoedwin@gmail.com>
2022-12-28 00:36:23 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Edwin Flatto";
+    int age = 25;
+    String state = "Florida";
+
+    System.out.println("Name: "+name+" Age: "+age+" State: "+state);
+
+
+  }
+}
